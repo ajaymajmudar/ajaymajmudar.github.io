4ab0b7e1d8b2185715ed6e1f4cdcd218562d4430
Ajay Majmudar <ajaymajmudar@gatech.edu>
2023-01-04 16:06:07 -0500

    deploy website

---
+++ build/precache-manifest.782d22208efdc20e9508465b41210cef.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284147a38ff3b17a2bbcc5f92b80206d",
+    "url": "/ajaymajmudar.github.io/index.html"
+  },
+  {
+    "revision": "93bbc518877f066f9c26",
+    "url": "/ajaymajmudar.github.io/static/css/3.7de71dab.chunk.css"
+  },
+  {
+    "revision": "489a5d431b7069d91a37",
+    "url": "/ajaymajmudar.github.io/static/css/4.d20e6211.chunk.css"
+  },
+  {
+    "revision": "31a179520d2114323fe4",
+    "url": "/ajaymajmudar.github.io/static/css/main.76e5ac24.chunk.css"
+  },
+  {
+    "revision": "25087b2b91d828e65641",
+    "url": "/ajaymajmudar.github.io/static/js/2.1ceb54c7.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/ajaymajmudar.github.io/static/js/2.1ceb54c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93bbc518877f066f9c26",
+    "url": "/ajaymajmudar.github.io/static/js/3.1766bfa9.chunk.js"
+  },
+  {
+    "revision": "489a5d431b7069d91a37",
+    "url": "/ajaymajmudar.github.io/static/js/4.a76fa1b9.chunk.js"
+  },
+  {
+    "revision": "31a179520d2114323fe4",
+    "url": "/ajaymajmudar.github.io/static/js/main.2307b362.chunk.js"
+  },
+  {
+    "revision": "bc634d77808db663b229",
+    "url": "/ajaymajmudar.github.io/static/js/runtime-main.8403a5f4.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/ajaymajmudar.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/ajaymajmudar.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6ff4561cbf6ce68e7fac555740cac274",
+    "url": "/ajaymajmudar.github.io/static/media/aflac.6ff4561c.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/ajaymajmudar.github.io/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/ajaymajmudar.github.io/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/ajaymajmudar.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/ajaymajmudar.github.io/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/ajaymajmudar.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "89580edf6fd8706284c1b1b21bf4331e",
+    "url": "/ajaymajmudar.github.io/static/media/ncr.89580edf.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/ajaymajmudar.github.io/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/ajaymajmudar.github.io/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/ajaymajmudar.github.io/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/ajaymajmudar.github.io/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/ajaymajmudar.github.io/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
