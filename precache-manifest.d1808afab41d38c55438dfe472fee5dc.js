9577af502ec745fc531654590c09115b859c1460
Ajay Majmudar <ajaymajmudar@gmail.com>
2023-01-30 18:44:02 -0500

    Updates

--- precache-manifest.d1808afab41d38c55438dfe472fee5dc.js
+++ precache-manifest.d1808afab41d38c55438dfe472fee5dc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "527f4382a19cb5617b907a629e590954",
+    "revision": "55fdf6604f800daf687df11f40deb783",
     "url": "/index.html"
   },
   {
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/4.c4782395.chunk.css"
   },
   {
-    "revision": "a8947edb1ec67597eecf",
+    "revision": "e173f480887a0d6e7a3a",
     "url": "/static/css/main.042edd29.chunk.css"
   },
   {
@@ -32,8 +32,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.a76fa1b9.chunk.js"
   },
   {
-    "revision": "a8947edb1ec67597eecf",
-    "url": "/static/js/main.68b325d1.chunk.js"
+    "revision": "e173f480887a0d6e7a3a",
+    "url": "/static/js/main.89548e9d.chunk.js"
   },
   {
     "revision": "a49287b4db2f38db881e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0cac154cdbb3da5c4636887487c6ff84.js"
+  "/precache-manifest.d1808afab41d38c55438dfe472fee5dc.js"
 );
 
 self.addEventListener('message', (event) => {
